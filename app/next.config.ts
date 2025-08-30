a2731be6bb1a6de6f7515c491d25b5cc747090db
VHC <vhc@MacBook-Pro-de-VHC-4.local>
2025-08-30 01:03:42 -0400

    ajuste 11:59

---
+++ app/next.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }, // opcional, sólo si también quieres que no bloquee por TS
+};
+
+export default nextConfig;
